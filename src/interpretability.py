b1eeebe7be4b39cce31b6e9953ea330e0bc525e8
tonibofarull <tonibofarull@gmail.com>
2021-03-23 21:15:23 +0100

    Importance of input/bottleneck with respect to output/class

--- src/interpretability.py
+++ src/interpretability.py
@@ -2,7 +2,12 @@ import torch
 import numpy as np
 from torch import optim
 
+# Shapley Values
+
 def shapley_sampling(x, model, feature, n_batches=1, batch_size=64):
+    """
+    Importance of input with respect the output
+    """
     length = x.shape[-1]
     sv = torch.zeros(length)
 
@@ -30,7 +35,10 @@ def shapley_sampling(x, model, feature, n_batches=1, batch_size=64):
     sv /= n_batches*batch_size
     return sv.numpy()
 
-def shapley_sampling_bottleneck(x, model, feature, baselines=None, n_batches=1, batch_size=64):
+def shapley_sampling_bottleneck_output(x, model, feature, baselines=None, n_batches=1, batch_size=64):
+    """
+    Importance of neuron with respect the output
+    """
     sv = torch.zeros(x.shape[-1])
     x = model.encoder(x.reshape(1,1,96), False) # bottleneck
     length = x.shape[-1]
@@ -59,6 +67,93 @@ def shapley_sampling_bottleneck(x, model, feature, baselines=None, n_batches=1, 
 
     sv /= n_batches*batch_size
     return sv.numpy()
+
+def shapley_sampling_bottleneck_class(x, model, feature, baselines=None, n_batches=1, batch_size=64):
+    sv = torch.zeros(7)
+    x = model.encoder(x.reshape(1,1,96), False) # bottleneck
+    length = x.shape[-1]
+    for _ in range(n_batches):
+        if baselines is None:
+            y = torch.zeros((batch_size, length)) # changed
+        else:
+            y = torch.tensor(baselines).reshape(1,24).repeat((batch_size,1))
+        O = np.array([np.random.permutation(length) for _ in range(batch_size)])
+        idx = np.where(O == feature)
+        Os = [O[i,:j] for i, j in zip(idx[0], idx[1])]
+
+        sel = torch.zeros((batch_size,length), dtype=torch.bool)
+        sel[np.concatenate([np.repeat(i,len(Os[i])) for i in range(batch_size)]), np.concatenate(Os)] = True
+
+        x2 = torch.where(sel, x, y)
+        x1 = x2.clone()
+        x1[:,feature] = x[:,feature]
+
+        with torch.no_grad():
+            v1 = model.classifier.get_probs(model.classifier(x1))
+            v2 = model.classifier.get_probs(model.classifier(x2))
+        sv += torch.sum(v1 - v2, axis=0)
+
+    sv /= n_batches*batch_size
+    return sv.numpy()
+
+def shapley_sampling_input_bottleneck(x, model, feature, baselines=None, n_batches=1, batch_size=64):
+    length = x.shape[-1]
+    sv = torch.zeros(model.bottleneck_nn)
+
+    x = x.reshape(1, length).repeat(batch_size, 1)
+    for _ in range(n_batches):
+        y = torch.rand((batch_size, length))
+        O = np.array([np.random.permutation(length) for _ in range(batch_size)])
+        idx = np.where(O == feature)
+        Os = [O[i,:j] for i, j in zip(idx[0], idx[1])]
+
+        sel = torch.zeros((batch_size,length), dtype=torch.bool)
+        sel[np.concatenate([np.repeat(i,len(Os[i])) for i in range(batch_size)]), np.concatenate(Os)] = True
+
+        x2 = torch.where(sel, x, y)
+        x1 = x2.clone()
+        x1[:,feature] = x[:,feature]
+
+        x1 = x1.reshape(-1,1,length)
+        x2 = x2.reshape(-1,1,length)
+        with torch.no_grad():
+            v1 = model.encoder(x1, False)
+            v2 = model.encoder(x2, False)
+        sv += torch.sum(v1 - v2, axis=0)
+
+    sv /= n_batches*batch_size
+    return sv.numpy()
+
+def shapley_sampling_input_class(x, model, feature, baselines=None, n_batches=1, batch_size=64):
+    length = x.shape[-1]
+    sv = torch.zeros(model.num_classes)
+
+    x = x.reshape(1, length).repeat(batch_size, 1)
+    for _ in range(n_batches):
+        y = torch.rand((batch_size, length))
+        O = np.array([np.random.permutation(length) for _ in range(batch_size)])
+        idx = np.where(O == feature)
+        Os = [O[i,:j] for i, j in zip(idx[0], idx[1])]
+
+        sel = torch.zeros((batch_size,length), dtype=torch.bool)
+        sel[np.concatenate([np.repeat(i,len(Os[i])) for i in range(batch_size)]), np.concatenate(Os)] = True
+
+        x2 = torch.where(sel, x, y)
+        x1 = x2.clone()
+        x1[:,feature] = x[:,feature]
+
+        x1 = x1.reshape(-1,1,length)
+        x2 = x2.reshape(-1,1,length)
+        with torch.no_grad():
+
+            v1 = model.classifier.get_probs(model(x1, False)[1])
+            v2 = model.classifier.get_probs(model(x2, False)[1])
+        sv += torch.sum(v1 - v2, axis=0)
+
+    sv /= n_batches*batch_size
+    return sv.numpy()
+
+# Feature Visualization
 
 def feature_visualization(model, neuron):
     """
